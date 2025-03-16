65d1d31638691b42f61dea6d0ffbf30736adc4b4
Sneha Datta <snehadatta431@gmail.com>
2025-03-16 09:02:48 +0530

    Fetch current location from android

--- src/services/current-location.js
+++ src/services/current-location.js
@@ -4,13 +4,21 @@
  * See the LICENSE file in the root directory for more information.
   */
 import { map } from "../components/map.js";
+import Marker from "../components/Marker/marker.js";
 
 
 export function fetchCurrentLocation() {
-      Android.getDeviceLocation();
-      map.locate({ setView: true, maxZoom: 16, enableHighAccuracy: true });
-    
+  if (window.Android) {
+    Android.getDeviceLocation();
+  } else {
+    map.locate({ setView: true, maxZoom: 16, enableHighAccuracy: true });
+  }
 }
-function getLocationData(location) {
-    map.locate({ setView: location, maxZoom: 16, enableHighAccuracy: true });
+
+window.updateDeviceLocation = function (lat, lon) {
+  map.setView([lat, lon], 16);
+  if (window.marker) {
+    window.map.removeLayer(window.marker);
+    window.marker = new Marker(map.getCenter()).addTo(map);
+   }
 }
